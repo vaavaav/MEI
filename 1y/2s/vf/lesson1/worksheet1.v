5a979542119785fcbe7a11b829fdbc6a2594be6f
vaavaav <vaavaav.pt@gmail.com>
2022-02-20 16:47:22 +0000

    Update VF : Worksheet 1

--- 1y/2s/vf/lesson1/worksheet1.v
+++ 1y/2s/vf/lesson1/worksheet1.v
@@ -1,4 +1,5 @@
-(* Exercise 2 *)
+(* Exercise 2 : Lógica Proposicional*)
+Section PL.
 
 Variables (A:Prop) (B:Prop) (C:Prop).
 
@@ -84,5 +85,121 @@ Proof.
         -- assumption.
   - apply ex2_4.
 Qed.
+
+End PL.
+
+(* Exercise 3: FOL*)
+
+Section FOL.
+
+Variables X Y: Set.
+Variables P Q R: X -> Prop.
+Variables P' : X -> Y -> Prop.
+
+(* Exercise 3.1 *)
+Lemma ex3_1 : (exists x, P x /\ Q x) -> (exists x, P x) /\ (exists x, Q x).
+Proof.
+  intro.
+  destruct H as [x H].
+  destruct H as [H1 H2].
+  split.
+   - exists x. assumption.
+   - exists x. assumption.
+Qed.
+
+(* Exercise 3.2 *)
+Lemma ex3_2 : (exists x, forall y, P' x y) -> forall y, exists x, P' x y.
+Proof.
+  intro.
+  destruct H as [x H].
+  intro.
+  exists x.
+  apply H.
+Qed.
+
+(* Exercise 3.3 *)
+Lemma ex3_3 : (exists x, P x) -> (forall x, forall y, P x -> Q y) -> forall y, Q y.
+Proof.
+ intros.
+ elim H.
+ intro.
+ apply H0.
+Qed.
+
+
+(* Exercise 3.4 *)
+Lemma ex3_4 : (forall x, Q x -> R x) -> (exists x, P x /\ Q x) -> exists x, P x /\ R x.
+Proof.
+ intros.
+ destruct H0 as [x H0].
+ exists x.
+ destruct H0 as [H01 H02].
+ split.
+ - assumption.
+ - apply H. assumption.
+Qed.
+
+(* Exercise 3.5 *)
+Lemma ex3_5 :  (forall x, P x -> Q x) -> (exists x, P x) -> exists y, Q y.
+Proof.
+ intros.
+ destruct H0 as [x H0].
+ exists x.
+ apply H.
+ assumption.
+Qed.
+
+(* Exercise 3.6 *)
+Lemma ex3_6 :  (exists x, P x) \/ (exists x, Q x) <-> (exists x, P x \/ Q x).
+Proof.
+ split.
+ - intro. destruct H as [H1|H2].
+   + destruct H1 as [x H1]. exists x. left. assumption.
+   + destruct H2 as [x H2]. exists x. right. assumption.
+ - intro. destruct H as [x H]. destruct H as [H1|H2].
+   + left. exists x. assumption.
+   + right. exists x. assumption.
+Qed.
+
+
+End FOL.
+
+
+(* Exercise 4: CL*)
+
+Section CL.
+
+Variables A B : Prop.
+Variables X : Set.
+Variables P : X -> Prop.
+
+Axiom law_of_excluded_middle : forall C : Prop, C \/ ~ C.
+
+(* Exercise 4.1 *)
+Lemma ex4_1 : ((A -> B) -> A) -> A.
+Proof.
+ intros.
+ destruct law_of_excluded_middle with A.
+ - assumption.
+ - apply H. contradiction.
+Qed.
+
+(* Exercise 4.2 *)
+Lemma ex4_2 : ~~A -> A.
+Proof.
+ intro.
+ destruct law_of_excluded_middle with A.
+ - assumption.
+ - contradiction.
+Qed.
+
+(* Exercise 4.3 *)
+Lemma ex4_3 : (~ forall x, P x) -> exists x, ~ P x.
+Proof.
+Qed.
+
+
+End CL.
+
 
 
