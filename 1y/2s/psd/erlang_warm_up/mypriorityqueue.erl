e643ab6b6a55f918cdcb742da28eb72a2ee12952
vaavaav <vaavaav.pt@gmail.com>
2022-03-25 09:06:56 +0000

    PSD : last changes to erlang warm up.

--- 1y/2s/psd/erlang_warm_up/mypriorityqueue.erl
+++ 1y/2s/psd/erlang_warm_up/mypriorityqueue.erl
@@ -1,8 +1,24 @@
 -module(mypriorityqueue).
 -export([create/0,enqueue/3,dequeue/1]).
 
-create() -> myqueue:create().
+create() -> maps:new().
 
-enqueue({Inorder,Reverse}, Item, Priority) -> {}.
+enqueue(Queues, Item, Priority) ->
+    Fun = fun(OldV) -> myqueue:enqueue(OldV,Item) end,
+    case maps:find(Priority,Queues) of
+        error -> maps:put(Priority,myqueue:create(),Queues);
+        _ -> true
+    end,
+    maps:update_with(Priority,Fun,Queues).
 
-dequeue({Inorder,Reverse}) -> {}.
+dequeue(Queues) -> 
+   case maps:keys(Queues) of
+     [] -> Queues;
+     Keys -> Min = lists:min(Keys), dequeue(Queues, Min)
+   end.
+
+dequeue(Queues, Priority) ->
+    case maps:find(Priority,Queues) of
+        {ok,Queue} -> {Item,NewQueue} = myqueue:dequeue(Queue), {Item, maps:put(Priority,NewQueue,Queues)};
+        error -> dequeue(Queues,Priority+1)
+    end.
